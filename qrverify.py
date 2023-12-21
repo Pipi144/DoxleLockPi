3f8e97859536198a92511d9c268c504d4a68f81a
Pipi144 <petertp144@gmail.com>
2023-12-21 11:28:54 +1100

    BS CHANGES

--- qrverify.py
+++ qrverify.py
@@ -1,29 +1,27 @@
 import requests
 import sys
 import json
-address = "http://192.168.1.109:9000"
+address = "http://192.168.1.103:8000"
 def verify_data(data):
-    print("DATA TYPE:", type(data))
     print("DATA:",data)
-    dump = json.dumps(data)
-    data_json = json.loads(dump)
-    print("DATA JSON:", data_json)
-    if data:
-        api_url = f"{address}/qr/verify/"
-        try:
-            print("DATA TYPE:", type(data_json))
-            response = requests.post(api_url, json = data_json, headers=head)
-            response.raise_for_status() #Raise exception for HTTP non 2--
-            
-            api_result = response.json()
-            print("API RESULT:", api_result)
-            return api_result
-        except requests.exceptions.RequestException as e:
-            print(f"APT REQUEST FAILED: {e}")
-            return False
-    else:
+    data_dict= json.loads(data)   
+    api_url = f"{address}/qr/verify/"
+    try:
+        response = requests.post(api_url, json=data_dict)
+        response.raise_for_status() #Raise exception for HTTP non 2--
+        
+        api_result = response.json()
+        print("API RESULT:", api_result)
+        return api_result
+    except requests.exceptions.RequestException as e:
+        print(f"APT REQUEST FAILED: {e}")
         return False
+    
+    
 if __name__ == "__main__":
+    response = requests.get("http://192.168.1.103:8000")
+    print("response", response)
+    
     if len(sys.argv) < 2:
         print("data passed in:", )
         sys.exit(1)
